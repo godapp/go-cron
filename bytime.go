6ae9cbc54ea138b25f730b734dc627cdad466b7f
yoojia.chen <yoojiachen@gmail.com>
2018-06-26 18:20:55 +0800

    Move bytime to a single file

---
+++ bytime.go
@@ -0,0 +1,24 @@
+package cron
+
+//
+// Author: 陈永佳 chenyongjia@parkingwang.com, yoojiachen@gmail.com
+// byTime is a wrapper for sorting the entry array by time
+// (with zero time at the end).
+//
+
+type byTime []*Entry
+
+func (s byTime) Len() int      { return len(s) }
+func (s byTime) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byTime) Less(i, j int) bool {
+	// Two zero times should return false.
+	// Otherwise, zero is "greater" than any other time.
+	// (To sort it at the end of the list.)
+	if s[i].Next.IsZero() {
+		return false
+	}
+	if s[j].Next.IsZero() {
+		return true
+	}
+	return s[i].Next.Before(s[j].Next)
+}
